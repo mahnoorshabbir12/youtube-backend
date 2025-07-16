4d10384546ac8fa8a94ed6e8da185b925fa9a5b1
mahnoorshabbir12 <tulipkardo123@gmail.com>
2025-07-16 08:59:21 +0500

    env and routes update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, getWatchHistory, loginUser, logoutUser, refreshAccessToken,registerUser, updateAccountDetails, updateAvatar, updateCoverImage }
+import { changeCurrentPassword, getCurrentUser, getWatchHistory, loginUser, logoutUser, refreshAccessToken,registerUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile }
 from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
